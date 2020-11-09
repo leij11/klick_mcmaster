d1f778fcba889cd10e20cde135ae0e8dfce38534
leij11 <leij11@hotmail.com>
2020-11-08 22:51:41 -0500

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import Discussion from "./components/Discussion";
+import Analytics from "./components/Analytics";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route path={"/Discussion"} component={Discussion} />
+          <Route path={"/Profile"} component={Profile} />
+          <Route path={"/Analytics"} component={Analytics} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Discussion.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Discussion = () => {
+  return <div>Discussion</div>;
+};
+
+export default Discussion;
