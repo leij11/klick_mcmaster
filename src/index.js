0578b950e00bf7b4a6acec646f79b244e40dc9ff
srixx18 <suri.jia@hotmail.com>
2020-11-11 16:54:54 -0500

    check git creds again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Profile from './App';
+import Profile from './Profile';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
